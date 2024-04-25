67efcfb3510955d60b717a6ec57287f87f6bd848
Waleska Silva <vale.sgonzalez2011@hotmail.com>
2024-04-24 23:47:42 -0400

    correccion pt5

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,32 +13,84 @@ class Propietario {
 class Animal extends Propietario {
     constructor(nombre, direccion, telefono, tipo){
         super(nombre, direccion, telefono)
-        this.tipo = tipo;
+        this.tipoAnimal = tipo; 
     }
     get tipo(){
-        return `El tipo de animal es un ${this.tipo}`
+        return `El tipo de animal es un: ${this.tipoAnimal}`
     }
 }
 
 class Mascota extends Animal{
-    constructor(nombrePropietario, direccionPropietario, telefonoPropietario, tipoAnimal, nombreMascota, 
+    constructor(nombrePropietario, direccionPropietario, telefonoPropietario, tipo, nombreMascota, 
         enfermedad){
-        super(nombrePropietario, direccionPropietario, telefonoPropietario, tipoAnimal);
-        this.nombreMascota = nombreMascota;
-        this.enfermedad = enfermedad;  //de momento dejare enfermedad en esta clase :S
+        super(nombrePropietario, direccionPropietario, telefonoPropietario, tipo);
+        this._nombreMascota = nombreMascota;
+        this._enfermedadMascota = enfermedad;
     }
-    get nombre(){
-        return this.nombreMascota;
+    get nombreMascota(){
+        return this._nombreMascota;
     }
-    set nombre(new_nombreMascota){
-        this.nombreMascota = new_nombreMascota;
+    set nombreMascota(newNombreMascota){
+        this._nombreMascota = newNombreMascota;
 
     }
-    //tengo que agregar un get & set a enfermedad si o si... despues veo donde lo pongo??
-    get enfermedad(){
-        return this.enfermedad;
+  
+    get enfermedadMascota(){
+        return this._enfermedadMascota;
     }
-    set enfermedad(new_enfermedad){
-        this.enfermedad = new_enfermedad;
+    set enfermedadMascota(newEnfermedad){
+        this._enfermedadMascota = newEnfermedad;
     }
 }
+
+
+//#region Form
+//selecciono el formulario
+let formulario = document.querySelector("form");
+//variable con elementos directos del form   
+let consulta = (event) => {
+    event.preventDefault();
+    //variables de las partes del formulario
+    let propietario = document.getElementById("propietario").value;
+    let telefono = document.getElementById("telefono").value;
+    let direccion = document.getElementById("direccion").value;
+    let nombreMascota = document.getElementById("nombreMascota").value;
+    let tipo = document.getElementById("tipo").value;
+    let enfermedad = document.getElementById("enfermedad").value;
+
+//ahora si, punto 5:
+    let mascota;
+    switch (tipo) {
+        case "perro":
+            mascota = new Mascota(
+                propietario, direccion, telefono, tipo, nombreMascota, enfermedad
+            );
+            break;
+        case "gato":
+            mascota = new Mascota(
+                propietario, direccion, telefono, tipo, nombreMascota, enfermedad
+            );
+            break;
+        case "conejo":
+            mascota = new Mascota(
+                propietario, direccion, telefono, tipo, nombreMascota, enfermedad
+            );
+            break;
+        default:
+            console.error("Tipo de animal no reconocido");
+            return;
+    }
+
+    function resultadoMascota(mascota) {
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `
+            <li>${mascota.datosPropietario()}</li>
+            <li>${mascota.tipo}, mientras que el nombre de la mascota es: ${mascota.nombreMascota}
+            y la enfermedad es: ${mascota._enfermedadMascota}</li>`;
+    }
+
+
+resultadoMascota(mascota);
+   
+};
+formulario.addEventListener("submit", consulta);
